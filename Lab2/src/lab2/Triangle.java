ecedbb027eb29deef15bd1e4a6c3d38e965bfb3d
Kevin Soares <kev.soares7@gmail.com>
2016-02-02 08:50:43 -0500

    Merging the source on my PC
    Pushing my local repo to the github servers :)

---
+++ Lab2/src/lab2/Triangle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Kevin Soares
+ */
+public class Triangle {
+    
+    // Triangle needs 3 sides
+    public int sideA;
+    public int sideB;
+    public int sideC;
+    
+    // Constructor takes in 3 values
+    public Triangle(int a, int b, int c)
+    {
+    }
+    
+    public boolean isTrinagle()
+    {
+        return true;
+    }
+    
+    public boolean isRight()
+    {
+        return true;
+    }
+}
