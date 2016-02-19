1e8e37b75c87ae55c6d244fcd11f7f92d08619f1
Ryan Guarasica <rguarascia@gmail.com>
2016-02-19 10:59:53 -0500

    Fixed.
    1) Changed public ints to private as-per assignment
    2) Added to check which side is the hypotenuse so that the Pythagorean
    theorem works correctly.

--- Lab2/src/lab2/Triangle.java
+++ Lab2/src/lab2/Triangle.java
@@ -10,86 +10,93 @@ package lab2;
  * @author Kevin Soares
  */
 public class Triangle {
-    
+
     /**
      * Side A of the triangle
      */
-    public int sideA;
+    private int sideA;
     /**
      * Side B of the triangle
      */
-    public int sideB;
+    private int sideB;
     /**
      * Side C of the triangle
      */
-    public int sideC;
-    
+    private int sideC;
+
     /**
-     * 
+     *
      * @param a input of side a of the triangle
      * @param b input of side b of the triangle
      * @param c input of side c of the triangle
      */
-    public Triangle(int a, int b, int c)
-    {
-        sideA = a;
-        sideB = b;
-        sideC = c;
+    public Triangle(int a, int b, int c) {
+        if (a > b && a > c) {
+            sideC = a;
+            sideA = c;
+            sideB = b;
+        } else if (b > a && b > c) {
+            sideC = b;
+            sideA = a;
+            sideB = c;
+        } else {
+            sideA = a;
+            sideB = b;
+            sideC = c;
+        }
     }
-    
+
     /**
      * Set the value of A and use this for getting in the main method after
+     *
      * @return sideA
      */
-    public int getA()
-    {
+    public int getA() {
         return sideA;
     }
-    
+
     /**
      * Set the value of B and use this for getting in the main method after
+     *
      * @return sideB
      */
-    public int getB()
-    {
+    public int getB() {
         return sideB;
     }
-    
+
     /**
      * Set the value of C and use this for getting in the main method after
+     *
      * @return SideC
      */
-    public int getC()
-    {
+    public int getC() {
         return sideC;
     }
-    
+
     /**
      * Check if the triangle is valid and return the correct boolean
+     *
      * @return true or false
      */
-    public boolean isTriangle()
-    {
-        if(sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA > sideB )
-        {
+    public boolean isTriangle() {
+        if (sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA > sideB) {
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
-    
+
     /**
-     * Calculate if triangle is right angled and send the appropriate boolean value
+     * Calculate if triangle is right angled and send the appropriate boolean
+     * value
+     *
      * @return true or false
      */
-    public boolean isRight()
-    {
-        double aPower = Math.pow((double)sideA, 2.0);
-        double bPower = Math.pow((double)sideB, 2.0);
-        double cPower = Math.pow((double)sideC, 2.0);
-        
+    public boolean isRight() {
+        double aPower = Math.pow((double) sideA, 2.0);
+        double bPower = Math.pow((double) sideB, 2.0);
+        double cPower = Math.pow((double) sideC, 2.0);
+
         return aPower + bPower == cPower;
     }
 }
